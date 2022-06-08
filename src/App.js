651dfbcb5ac75bf83bda48518e85c84d16f834a5
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-08 14:47:52 -0300

    Dev: Table included in FiltersProvider.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Table from './components/Table';
+import FiltersProvider from './context/Filter';
 
 function App() {
   return (
-    <Table />
+    <FiltersProvider>
+      <Table />
+    </FiltersProvider>
   );
 }
 
